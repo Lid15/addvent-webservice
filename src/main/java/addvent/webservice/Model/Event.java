2ad0e8efde33f49f4159a0432d814b65009c2912
Kári Snær Kárason <kariskarason@gmail.com>
2018-03-14 18:48:01 +0000

    Bætti við leið til að setja inn Event 3

--- src/main/java/addvent/webservice/Model/Event.java
+++ src/main/java/addvent/webservice/Model/Event.java
@@ -16,7 +16,7 @@ import java.util.Set;
 @Entity(name="Event")
 public class Event {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     /*@Version
